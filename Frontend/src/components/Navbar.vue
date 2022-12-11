5263135d4441d78263eec8dba1de1cc1f9bdb652
ramit110 <Ramit110@users.noreply.github.com>
2022-12-11 20:36:07 +0000

    Missed a file

---
+++ Frontend/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+
+<script setup lang="ts">
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">IoT Core Thing</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample05">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home/Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">...</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">...</a>
+                <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">...</a></li>
+                </ul>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    <p />
+</template>
